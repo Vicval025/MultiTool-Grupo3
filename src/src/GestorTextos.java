f90073c3f8f2a0ee609d519bfa9a62d368b4f900
ivagarcer <ivagarcer@alu.edu.gva.es>
2025-12-16 09:22:30 +0100

    Primer commit Creación del menú

--- src/src/GestorTextos.java
+++ src/src/GestorTextos.java
@@ -1,2 +1,15 @@
-public class GestorTextos {
+public class GestorTextos{
+    public static void tareas(){
+        Scanner sc = new Scanner(System.in);
+        String [] tarea_array = new String[15];
+        int eleccion_usuario;
+        int contador=0;
+        do{
+        System.out.print("1.Añadir tarea");
+        System.out.print("2.Listar tarea");
+        System.out.print("3.Buscar palabra");
+        System.out.print("0.Volver");
+
+        }while(eleccion_usuario!=0);
+    }
 }
