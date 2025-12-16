a4258a140eceaa6034dec2e6178936d01e1e093f
ivagarcer <ivagarcer@alu.edu.gva.es>
2025-12-16 09:39:57 +0100

    Segundo commit Bucle si número no está en el rango

--- src/src/GestorTextos.java
+++ src/src/GestorTextos.java
@@ -5,10 +5,17 @@ public class GestorTextos{
         int eleccion_usuario;
         int contador=0;
         do{
-        System.out.print("1.Añadir tarea");
-        System.out.print("2.Listar tarea");
-        System.out.print("3.Buscar palabra");
-        System.out.print("0.Volver");
+            do {
+                System.out.println("1.Añadir tarea");
+                System.out.println("2.Listar tarea");
+                System.out.println("3.Buscar palabra");
+                System.out.println("0.Volver");
+                System.out.print("Elige una opción: ");
+                eleccion_usuario = sc.nextInt();
+                sc.nextLine();
+            }while(eleccion_usuario<0 || eleccion_usuario>3)
+
+
 
         }while(eleccion_usuario!=0);
     }
