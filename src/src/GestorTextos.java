8d53047742faf8c0ac1ffe8ff4e1e28a2582311c
ivagarcer <ivagarcer@alu.edu.gva.es>
2025-12-16 11:38:32 +0100

    Tercer commit Correción e implementación de métodos

--- src/src/GestorTextos.java
+++ src/src/GestorTextos.java
@@ -5,7 +5,6 @@ public class GestorTextos{
         int eleccion_usuario;
         int contador=0;
         do{
-            do {
                 System.out.println("1.Añadir tarea");
                 System.out.println("2.Listar tarea");
                 System.out.println("3.Buscar palabra");
@@ -13,12 +12,37 @@ public class GestorTextos{
                 System.out.print("Elige una opción: ");
                 eleccion_usuario = sc.nextInt();
                 sc.nextLine();
-            }while(eleccion_usuario<0 || eleccion_usuario>3)
-            switch (eleccion_usuario){
+            switch (eleccion_usuario) {
+                case 1:
+                    Añadir_lista(eleccion_usuario);
+                    break;
+                case 2:
+                    Listar_tarea(eleccion_usuario);
+                    break;
+                case 3:
+                    Buscar_palabra(eleccion_usuario);
+                    break;
+                case 0:
+                    System.out.println("Volver");
+                default:
+                    System.out.println("Número inválido");
 
             }
-
-
         }while(eleccion_usuario!=0);
+    }
+    static void Añadir_lista(int eleccion_usuario){
+        if (eleccion_usuario==1){
+
+        }
+    }
+    static void Listar_tarea(int eleccion_usuario){
+        if (eleccion_usuario==2){
+
+        }
+    }
+    static void Buscar_palabra(int eleccion_usuario){
+        if (eleccion_usuario==3){
+
+        }
     }
 }
