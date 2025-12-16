4c75f498135a6e37727100149c4f7c2b01e929c7
Victor <vicvaldvc@gmail.com>
2025-12-16 08:42:07 +0100

    Creacion archivos java vacios

---
+++ src/src/Main.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("MENÚ");
+        System.out.println("1-Gestor de números");
+        System.out.println("2-Conversor Multitipo");
+        System.out.println("3-Analizador de textos");
+        System.out.println("4-Gestor de tareas");
+        System.out.println("5-Adivinar números");
+    }
+}
